0c041f3b29046342555ddd134a7e85260aa6e7f2
William Huster <william@thinknimble.com>
2025-10-02 13:11:23 -0400

    Remove debug logging and add PR preview support

--- _plugins/environment_url.rb
+++ _plugins/environment_url.rb
@@ -1,24 +1,20 @@
 # Dynamically set site.url from environment variable
 # Allows Cloudflare Pages to use the deployment URL for previews
 Jekyll::Hooks.register :site, :after_init do |site|
-  # Debug: print environment variables
-  puts "DEBUG: CF_PAGES_URL = #{ENV['CF_PAGES_URL']}"
-  puts "DEBUG: CF_PAGES_BRANCH = #{ENV['CF_PAGES_BRANCH']}"
-  puts "DEBUG: URL = #{ENV['URL']}"
-  puts "DEBUG: Current site.url = #{site.config['url']}"
-
   # Override URL based on environment
   if ENV['CF_PAGES_BRANCH']
     # Cloudflare Pages deployment
     if ENV['CF_PAGES_BRANCH'] == 'main'
       site.config['url'] = 'https://tn-research.pages.dev'
       site.config['baseurl'] = ''
+    else
+      # PR preview - use CF_PAGES_URL
+      site.config['url'] = ENV['CF_PAGES_URL']
+      site.config['baseurl'] = ''
     end
   elsif ENV['JEKYLL_URL']
+    # Manual override via environment variable
     site.config['url'] = ENV['JEKYLL_URL']
     site.config['baseurl'] = ''
   end
-
-  puts "DEBUG: Final site.url = #{site.config['url']}"
-  puts "DEBUG: Final site.baseurl = #{site.config['baseurl']}"
 end
